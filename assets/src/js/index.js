18f434a953018acf65c8ad9bd6118f61bcc9ecf6
lyoto07 <guilherme-lira@outlook.com.br>
2022-11-09 00:13:49 -0300

    correção de bug do menu

--- assets/src/js/index.js
+++ assets/src/js/index.js
@@ -28,7 +28,7 @@ function menuMobile() {
 }
 
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.width = "75%";
 
 }
 
